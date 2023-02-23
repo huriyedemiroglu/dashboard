85deb75bfdea4a7b93d35c2538b54af4efc0d3d9
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-02-23 11:40:14 -0600

    Navbar created

--- src/App.js
+++ src/App.js
@@ -52,8 +52,8 @@ const App = () => {
           )}
           <div
             className={
-              activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+              activeMenu 
+              ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
                 : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
             }
           >
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -66,4 +66,5 @@ const Sidebar = () => {
     </div>
   );
 };
+export default Sidebar;
 
