710fda0aad8e730d74e37e44a028316bc6414121
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-03-09 09:00:20 -0600

    contex provider and theme setting renewed

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,19 @@ export const ContextProvider=({children})=>{
 const [activeMenu, setactiveMenu] = useState(true);
 const [isClicked, setIsClicked] =useState(initialState)
 const [screenSize, setScreenSize]=useState(undefined)
+const [currentColor,setCurrentColor]=useState('#03C9D7')
+const [currentMode, setcurrentMode] = useState('light')
+const [themeSettings, setThemeSettings] = useState(false)
+
+const setMode=(e)=>{
+  setcurrentMode(e.target.value)
+  localStorage.setItem('themeMode', e.target.value)
+  setThemeSettings(false)
+}
+const setColor=(e)=>{
+  setCurrentColor(e.target.value)
+  localStorage.setItem('ColorMode', e.target.value)
+}
 
 const handleClick=(clicked)=>{
   setIsClicked({...initialState, [clicked]: true})
@@ -29,7 +42,10 @@ value={{
   setIsClicked,
   handleClick,
   screenSize,
-  setScreenSize
+  setScreenSize,
+  currentColor, currentMode,
+  setColor,setMode,
+  themeSettings, setThemeSettings
   }}
 >
 {children}
