0e7aaea607a2e0527f112006eebadee40a6c707b
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-03-09 10:31:18 -0600

    SparkLine comp changed

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
-const SparkLine = ({id,height,width, color, data, type, currentColor}) => {
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
   return (
     <SparklineComponent
     id={id}
@@ -13,7 +15,7 @@ const SparkLine = ({id,height,width, color, data, type, currentColor}) => {
     border={{ color: currentColor, width: 2 }}
     tooltipSettings={{
       visible: true,
-      format: '${x} : data ${yval}',
+    format: '${x} : data ${yval}',
       trackLineSettings: {
         visible: true,
       },
@@ -27,6 +29,7 @@ const SparkLine = ({id,height,width, color, data, type, currentColor}) => {
     <Inject services={[SparklineTooltip]} />
   </SparklineComponent>
   )
+}
 }
 
 export default SparkLine
