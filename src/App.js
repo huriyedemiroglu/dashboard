f93c224127421d01750ac465579aa6ec3551928b
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-03-22 10:26:10 -0500

    deployed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
-import { useStateContext } from './contexts/ContextProvider';
+import { useStateContext } from './contexts/ContextProvider'
 
 const App = () => {
   const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
